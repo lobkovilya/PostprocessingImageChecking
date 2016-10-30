56d87dd02f5ac6432122ba1e552b758cdb8ee8cc
TanyaIgnatenko <alisainy@gmail.com>
2016-10-30 16:24:30 +0600

    Did minor fix.

--- src/Metadata analysis/print_exif.py
+++ src/Metadata analysis/print_exif.py
@@ -1,10 +1,10 @@
 from PIL import Image
 from PIL.ExifTags import TAGS
 
-def print_exif(imagePath):
-    image = Image.open(imagePath)
+def print_exif(image):
     exif_data = image._getexif()
     for tag, value in exif_data.items():
         print("%s: %s" % (TAGS[tag], value))
 
-print_exif("1.jpg")
+image = Image.open("1.jpg")
+print_exif(image)
