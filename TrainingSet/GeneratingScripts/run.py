092de9ffebfb16f331d95af3529dd66411589b1b
TanyaIgnatenko <alisainy@gmail.com>
2017-04-17 08:33:39 +0600

    Fixed bug.

--- TrainingSet/GeneratingScripts/run.py
+++ TrainingSet/GeneratingScripts/run.py
@@ -3,11 +3,11 @@ from load_save_script import save_to_file, load_from_file
 from filter_applier import FilterApplier
 import itertools
 
-urls_file = "txt/fishes_urls.txt"
-modified_urls_file = "txt/modified_fishes_urls.txt"
+urls_file = "urls/fishes_urls.txt"
+modified_urls_file = "urls/modified_fishes_urls.txt"
 applier = FilterApplier()
 image_descriptors = load_from_file(urls_file)
 for desc in itertools.islice(image_descriptors, 0, 3):
     image = desc.get_image()
-    image_descriptors = upload_images_to_cloud(applier.apply(image))
-    save_to_file(modified_urls_file, image_descriptors)
+    modified_image_descriptors = upload_images_to_cloud(applier.apply(image))
+    save_to_file(modified_urls_file, modified_image_descriptors)
